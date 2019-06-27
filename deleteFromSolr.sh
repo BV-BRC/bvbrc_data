ff67073ee8f6e9c090398dd662de068062db8f6e
mshukla1 <mshukla1@gmail.com>
2019-06-27 10:50:42 -0500

    Removed deleteFromSolr.sh

--- deleteFromSolr.sh
+++
@@ -1,2 +0,0 @@
-echo "$PATRIC_SOLR/$1/update?stream.body=<delete><query>$2:$3</query></delete>&commit=true"
-curl "$PATRIC_SOLR/$1/update?stream.body=<delete><query>$2:$3</query></delete>&commit=true"
